96e8060809f61b09713f7cd1d5cdc8eab94c6c91
CAPELLAX02 <ahmtatar@gmail.com>
2024-12-02 20:41:34 +0300

    Create abstract jpa entity class

---
+++ src/main/java/com/capellax/whatsapp_backend/shared/jpa/AbstractAuditingEntity.java
@@ -0,0 +1,30 @@
+package com.capellax.whatsapp_backend.shared.jpa;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public abstract class AbstractAuditingEntity<T> {
+
+    public abstract T getId();
+
+    @CreatedDate
+    @Column(name = "created_date", updatable = false)
+    private Instant createdDate = Instant.now();
+
+    @LastModifiedDate
+    @Column(name = "last_modified_date")
+    private Instant lastModifiedDate = Instant.now();
+
+}
